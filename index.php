b1914e0cbadfae42296223274239de8b142b27d1
Kaique Nocetti <kaiquenocetti07@gmail.com>
2023-07-28 20:01:12 -0300

    Adicionando todos arquivos

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<?php $raiz = ""; include('user/connsql.php'); ?>
+<?php //include('panel/select.php'); ?>
+<html lang="pt-br">
+<head>
+<?php $raiz = ""; include('components/head.php'); ?>
+</head>
+<body>
+<header>
+<?php include 'components/header.php';  ?>
+</header>
+<div class="btn-whats"><a href="https://api.whatsapp.com/send?phone=552195131494">
+	<img src="img/whatsapp.png" alt="Fale comigo no whatsapp"></a>
+</div>
+<section class="banner">
+	<div class="imgBx">
+		<?php 
+			$sql = "SELECT im.id_img_imovel, im.id_imovel, i.nome_imovel"
+				." FROM img_imovel im"
+				." LEFT JOIN imovel i ON im.id_imovel = i.id_imovel"
+				." WHERE tipo_imagem = 1";
+			$rs = $conn->query($sql);
+			$row = $rs->fetch_assoc();
+			print "<img src='img/00" . $row['id_imovel'] . "/00" . $row['id_img_imovel'] . ".jpg' alt='Foto do " . $row['nome_imovel'] . "' class='active'>";
+
+			while($row = $rs->fetch_assoc()){
+				print "<img src='img/00" . $row['id_imovel'] . "/00" . $row['id_img_imovel'] . ".jpg' alt='Foto do " . $row['nome_imovel'] . "'>";
+			} 
+		?>
+	</div>
+	<div class="contentBx">
+		<?php 
+			$sql = "SELECT nome_imovel, url_imovel, descricao_imovel"
+				." FROM imovel";
+			$rs = $conn->query($sql);
+			$row1 = $rs->fetch_assoc();
+			print "<div class='active'>";
+				print "<h2>" . $row1['nome_imovel'] . "</h2>";
+				print "<p>" . $row1['descricao_imovel'] . "</p>";
+				print "<a href='" . $row1['url_imovel'] . "'>Saiba mais</a>";
+			print "</div>";
+
+			while($row1 = $rs->fetch_assoc()){
+				print "<div>";
+					print "<h2>" . $row1['nome_imovel'] . "</h2>";
+					print "<p>" . $row1['descricao_imovel'] . "</p>";
+					print "<a href='" . $row1['url_imovel'] . "'>Saiba mais</a>";
+				print "</div>";			
+			} 
+		?>
+	</div>
+	<ul class="controls">
+		<li onclick="nextSlide(); nextSlideText();"></li>
+	</ul>
+</section>
+<section class="imoveis" id="imoveis">
+	<h2>Principais Lançamentos</h2>
+	<div class="swiper mySwiper">
+      		<div class="swiper-wrapper">
+
+			  <?php 
+				$sql = "SELECT i.id_imovel, i.nome_imovel, i.url_imovel, i.metragem_imovel, i.qtd_quartos, im.id_img_imovel  "
+				. " FROM imovel i"
+				. " LEFT JOIN img_imovel im ON i.id_imovel = im.id_imovel"
+				. " WHERE im.tipo_imagem = 2";
+				$rs = $conn->query($sql);
+
+				while($row2 = $rs->fetch_assoc()){
+					print "<div class='swiper-slide'>";
+						print "<div class='itemBox'>";
+							print "<img src='img/00" . $row2['id_imovel'] . "/00" . $row2['id_img_imovel'] . ".jpg' alt='" . $row2['nome_imovel'] . "'>";
+							print "<div class='content'>";
+								print "<h2>" . $row2['nome_imovel'] . "</h2>";
+								print "<p>" . $row2['metragem_imovel'] . "</p>";
+								print "<p>" . $row2['qtd_quartos'] . "</p>";
+								print "<a href='" . $row2['url_imovel'] . "'>Saiba mais</a>";
+							print "</div>";			
+						print "</div>";			
+					print "</div>";			
+				} 
+			?>
+      	</div>
+		<div class="swiper-button-next"></div>
+		<div class="swiper-button-prev"></div>
+  	</div>
+</section>
+<section class="about">
+	<div class="imgSidebar">
+		<img src="img/AJF.jpg" alt="Foto do corretor dono do AJF.">
+	</div>
+	<div class="contentSx">
+		<div>
+			<h4><span>AJF</span> Imóveis</h4>
+			<h2>Anderson Justino Ferreira</h2>
+			<p>Sou um corretor de imóveis altamente qualificado e experiente, <b>com uma sólida reputação no mercado imobiliário.</b> Sou conhecido por minha capacidade de entender as necessidades e desejos dos meus clientes, e por oferecer um serviço personalizado e profissional. Com uma vasta rede de contatos e uma compreensão profunda do mercado imobiliário local, sou capaz de ajudar meus clientes a <b>encontrar a propriedade perfeita para sua necessidade.</b></b> Sou altamente comprometido em fornecer um atendimento excepcional e é amplamente respeitado por minha ética de trabalho, integridade e transparência. Comigo, Anderson Justino Ferreira, <b>você pode ter certeza de que está em boas mãos para comprar ou vender um imóvel.</b></p>
+			<h4>CRECI 35113-RJ</h4>
+		</div>
+	</div>
+</section>
+<section class="info">
+	<div class="main-container">
+	  <div class="heading">
+	    <h1 class="heading_title">Morar no centro do Rio</h1>
+	    <p class="heading_credits"><span class="heading_link">tem mais vantagens do que você imagina.</span></p>
+	  </div>
+	  <div class="cards">
+	    <div class="card">
+				<div class="card_icon"><i class="fa-solid fa-masks-theater"></i></div> 
+				<h2 class="card_title">Serviços e Lazer
+				</h2>
+				<p>Morar no centro do Rio de Janeiro permite fácil acesso a uma grande variedade de serviços e opções de lazer. <b style="color: var(--laranja);">Há uma grande variedade de restaurantes, cafés, bares e lojas que atendem a uma ampla gama de gostos e orçamentos</b>. Além disso, existem muitos pontos turísticos históricos e culturais na região, como o Museu Nacional de Belas Artes, a Biblioteca Nacional e o Teatro Municipal.</p>
+	    </div>
+	    <div class="card">
+				<div class="card_icon"><i class="fa-solid fa-bus"></i></div>
+				<h2 class="card_title">Facilidade de transporte
+				</h2>
+				<p>Os lançamentos da região são bem servidos por <b style="color: var(--laranja);">transporte público incluindo metrô, ônibus, trem e VLT na porta</b>, o que torna fácil se locomover pela cidade. Além disso, muitas empresas têm suas sedes no centro da cidade, o que significa que é possível chegar ao trabalho facilmente, sem precisar se deslocar por grandes distâncias.</p>
+	    </div>
+	    <div class="card">
+				<div class="card_icon"><i class="fa-solid fa-building-circle-check"></i></div>
+				<h2 class="card_title">Infraestrutura completa.
+				</h2>
+				<p>Como uma das áreas mais desenvolvidas e importantes da cidade, o centro do Rio de Janeiro possui uma infraestrutura completa.<b style="color: var(--laranja);"> Há muitos hospitais, clínicas, escolas, universidades e centros de compras na região, tornando a vida mais conveniente e permitindo fácil acesso a serviços importantes.</b> Além disso, o centro da cidade é muito bem policiado, o que torna a região mais segura e tranquila para os moradores.</p> 
+	  	</div>
+	  	<div class="card">
+				<div class="card_icon"><i class="fa-solid fa-check-double"></i></div>
+				<h2 class="card_title">Retorno sobre o investimento.
+				</h2>
+				<p>O retorno sobre o investimento em imóveis no centro do Rio de Janeiro pode ser uma opção atrativa para os investidores. <b style="color: var(--laranja);">O centro da cidade é uma área de grande importância histórica, cultural e comercial, o que pode contribuir para a valorização dos imóveis nessa região.</b></p> 
+	  	</div>
+	  	<div class="card">
+				<div class="card_icon"><i  class="fa-solid fa-building-circle-check"></i></div>
+				<h2 class="card_title">Extensão da Zona Sul
+				</h2>
+				<p><b style="color: var(--laranja);">O centro do Rio de Janeiro, em particular a região da Orla Conde, pode ser considerado uma extensão natural da zona sul da cidade.</b> Essa área da cidade está localizada ao longo da bela orla marítima, estendendo-se desde a Praça Mauá até a Praça XV, abrangendo pontos turísticos famosos, como o Museu do Amanhã e a região portuária revitalizada.</p> 
+	  	</div>
+		</div>
+	</div>
+</section>
+<?php 
+	$mensagem = "Gostou de um imóvel? Deixe seu melhor contato, vou entrar em contato diretamente com você.";
+	$pagina = "Inicial";
+	include('components/form-contato.php'); 
+?>
+<?php include('components/footer.php'); ?>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+<script src="https://www.google.com/recaptcha/api.js?render=6Lehc_IlAAAAAK7617ohtDnlST0AjHvi9FMfJRcj"></script>
+<script src="js/jquery-min.js"></script>
+<script src="js/jquery-mask-money.js"></script>
+<script src="js/recaptcha.js"></script>
+<script src="js/swiper.js"></script>
+<script src="js/slide.js"></script>
+<script src="js/events.js"></script>
+</body>
+</html>
---
+++ vargas.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>AJF Imóveis | Residencial Vargas</title>
+	<?php include'head_imovel.php'; ?>
+</head>
+<body>
+<header>
+	<?php include'header.php' ?>
+</header>
+<section class="banner">
+	<div class="imgBx">
+		<img src="img/rdsv/01.jpg" alt="Residencial Vargas cobertura.">
+	</div>
+	<div class="contentBx">
+		<div class="info">
+			<h2>Residencial Vargas</h2>
+			<p>Você no topo do Rio.</p>
+			<span class="destaque">Lançamento</span>
+			<a href="https://api.whatsapp.com/send?phone=552195131494" class="destaque">Falar com o corretor</a>
+		</div>
+	</div>
+</section>
+<section class="p50" style="background: var(--branco);">
+	<div class="swiper mySwiper2">
+
+		<div class="swiper-wrapper">
+
+			<div class="swiper-slide">
+
+				<div class="img-left w30">
+					<img src="img/rdsv/01.jpg">
+				</div>
+
+				<div class="dsc-right w70">
+					
+					<div class="swiper mySwiper">
+						<div class="swiper-wrapper">
+							<div class="swiper-slide sombra">
+								<img src="img/rdsv/03.jpg" alt="Residencial Vargas apresentação.">
+							</div>
+							<div class="swiper-slide sombra">
+								<img src="img/rdsv/05.jpg" alt="Residencial Vargas apresentação.">
+							</div>
+							<div class="swiper-slide sombra">
+								<img src="img/rdsv/06.jpg" alt="Residencial Vargas apresentação.">
+							</div>
+							<div class="swiper-slide sombra">
+								<img src="img/rdsv/10.jpg" alt="Residencial Vargas apresentação.">
+							</div>
+							<div class="swiper-slide sombra">
+								<img src="img/rdsv/11.jpg" alt="Residencial Vargas apresentação.">
+							</div>
+							<div class="swiper-slide sombra">
+								<img src="img/rdsv/08.jpg" alt="Residencial Vargas apresentação.">
+							</div>
+						</div>
+						<div class="swiper-button-next"></div>
+						<div class="swiper-button-prev"></div>
+						<!--div class="swiper-pagination"></div-->
+					</div>
+
+					<div class="dsc-bottom">
+						<div class="dsc-text">
+							<h2>Projeto</h2>
+							<p>O Vargas 1140 Centro é o mais novo lançamento do Cury Construtora, na Av. Presidente Vargas no Centro do Rio de Janeiro.</p>
+						</div>
+					</div>
+
+				</div>
+
+				</div>
+
+				<div class="swiper-slide">
+					<div class="img-left w30">
+						<img src="img/rdsv/13.jpg">
+					</div>
+
+				<div class="dsc-right w70">
+					
+					<div class="swiper mySwiper">
+						<div class="swiper-wrapper">
+							<div class="swiper-slide sombra">
+								<img src="img/rdsv/15.jpg" alt="Residencial Vargas apresentação.">
+							</div>
+							<div class="swiper-slide sombra">
+								<img src="img/rdsv/14.jpg" alt="Residencial Vargas apresentação.">
+							</div>
+						</div>
+						<div class="swiper-button-next"></div>
+						<div class="swiper-button-prev"></div>
+						<!--div class="swiper-pagination"></div-->
+					</div>
+
+					<div class="dsc-bottom">
+						<div class="dsc-text">
+							<h2>Planta</h2>
+							<p>Apartamentos tipo Studio, medindo 32,35 m²
+Apartamentos de 1 quarto, com 37,54 m² 
+Apartamentos de  2 quartos, com 54,45 m²</p>
+						</div>
+					</div>
+				</div>
+
+				</div>
+				<div class="slide-pagination d-flex"></div>
+			</div>	
+</section>
+<section class="js-ard w100 p20 m-column m-center">
+	<h1 style="color: var(--laranja);">Apartamentos do Vargas 1140</h1>
+	<div class="box-icon">
+		<i class="fa fa-solid fa-bed"></i>
+		<h1>studio e 2 quartos</h1>
+	</div>
+
+	<div class="box-icon">
+		<i class="fa fa-solid fa-sink"></i>
+		<h1>1 banheiro</h1>
+	</div>
+
+	<div class="box-icon">
+		<i class="fa fa-regular fa-square-full"></i>
+		<h1>Aptos de 29 e 54 m²</h1>
+	</div>
+</section>
+<section class="js-ard m-column p20 text-d" style="background: var(--gradient);">
+	<div class="p20 m-center">
+		<h2>FALE COM O CORRETOR</h2>
+		<p>ONLINE SOBRE O IMÓVEL</p>	
+	</div>
+	<div class="js-center al-center mobile-column">
+		<a href="https://api.whatsapp.com/send?phone=552195131494" class="fale-conosco-btn">WHATSAPP <i class="fa-brands fa-whatsapp"></i></a>
+		<a href="https://api.whatsapp.com/send?phone=552195131494" class="fale-conosco-btn">TELEFONE <i class="fa-solid fa-phone"></i></a>
+	</div>	
+</section>
+<section class="js-btw m-column text-l w100 p50">
+	<div class="w50">
+		<h1><i class="fa-solid fa-location-dot" style="color: var(--laranja);"></i> Av. Presidente Vargas, Centro, Rio de Janeiro - RJ</h1>
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.272482102597!2d-43.18868318540051!3d-22.903318443504514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x997f800551b933%3A0x80fabee0c82a7baa!2sResidencial%20Vargas%201140%20-%20Cury!5e0!3m2!1spt-BR!2sbr!4v1670706814014!5m2!1spt-BR!2sbr" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+	</div>
+	<div class="w50 text-l p0-20">
+		<h2>Localização do Vargas 1140 Centro</h2>
+		<p>O Vargas 1140 Centro fica na avenida que conta a história da cidade maravilhosa, a avenidade Presidente Vargas.</p>
+		<p>E o mais incrível é que o Vargas 1140 Residencial chega para compor esse cenário de uma maneira inédita e surpreendente, trazendo com ele um novo olhar para a vida no Centro da Cidade.</p>
+		<p>Com o Vargas 1140 Centro, viver no Centro do Rio de Janeiro é sinômino de qualidade de vida, pois, incorpora no dia a dia do carioca contemporâneo o que há de mais precioso nos dias de hoje: tempo. </p>
+		<p>Para ilustrar melhor o que estamos falando, conheça os vizinhos do Vargas 1140 Centro: </p>
+		<p>Morar pertinho de tudo é ganhar tempo e, com isso, ter uma vida com mais qualidade.</p>
+		<ul>
+			<li><i class="fa-solid fa-check"></i>O Saara e suas 119 lojas</li>
+			<li><i class="fa-solid fa-check"></i>18 shoppings</li>
+			<li><i class="fa-solid fa-check"></i>24 padarias</li>
+			<li><i class="fa-solid fa-check"></i>35 restaurantes</li>
+			<li><i class="fa-solid fa-check"></i>25 supermercados</li>
+			<li><i class="fa-solid fa-check"></i>21 escolas</li>
+			<li><i class="fa-solid fa-check"></i>37 bancos</li>
+			<li><i class="fa-solid fa-check"></i>17 hospitais</li>
+
+			<li><i class="fa-solid fa-check"></i>32 farmácias</li>
+			<li><i class="fa-solid fa-check"></i>23 academias</li>
+			<li><i class="fa-solid fa-check"></i>13 petshops</li>
+			<li><i class="fa-solid fa-check"></i>20 salões de beleza</li>
+		</ul>
+	</div>
+</section>
+<?php 
+
+include('calculadora-financiamento.php'); 
+$mensagem = "Gostou do Residencial Vargas? Preencha o formulário, vou entrar em contato diretamente com você.";
+$pagina = "Residencial Vargas";
+include'form-contato.php';
+include'footer.php'; 
+include'js.php';
+include'js-send.php'; 
+
+?>
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<!-- Initialize Swiper -->
+<script>
+  var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  mousewheel: true,
+  keyboard: true,
+  });
+
+  var menu = ['PROJETO', 'PLANTA']
+  var swiper2 = new Swiper(".mySwiper2", {
+  	spaceBetween: 30,
+  	effect: "fade",
+    pagination: {
+      el: ".slide-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (menu[index]) + '</span>';
+      },
+    },
+  });
+</script>
+</body>
+</html>
