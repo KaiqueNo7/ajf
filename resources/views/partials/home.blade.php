6af71fadcc0856984b213ea7835b40955109dce3
Kaique Nocetti <kaiquenocetti07@gmail.com>
2025-03-07 00:31:57 -0300

    Update home.blade.php

--- resources/views/partials/home.blade.php
+++ resources/views/partials/home.blade.php
@@ -1,49 +1,52 @@
-@if(isset($properties[0]))
-    <div class="swiper w-full h-screen" id="slide-home">
-        <div class="swiper-wrapper">
-            @foreach ($properties as $property)
-            <div class="swiper-slide">
-                <div class="relative h-[31rem] w-full rounded-2xl shadow-lg overflow-hidden">
-                    <img src="{{ asset('storage/' . $property->image) }}" alt="" 
-                        class="absolute w-full h-full left-0 top-0 object-cover z-0">
-                    
-                    <div class="absolute bottom-0 left-0 w-full h-96 z-10 bg-gradient-to-t from-slate-900 to-transparent"></div>
-                    
-                    <div class="absolute bottom-10 left-10 z-20 text-white">
-                        <h1 class="text-5xl font-light">{{ $property->name }}</h1>
-                        <p class="mt-2">{{ $property->project }}</p>
-                        <x-button-link href="imovel/{{ normalizeString($property->name) }}/{{ $property->id }}">Saiba mais</x-button-link>
+@if(!empty($properties[0]))
+    <section class="w-full min-h-screen relative flex justify-start items-end px-12 py-10 pb-5">
+        <div class="swiper w-full" id="slide-home">
+            <div class="swiper-wrapper">
+                @foreach ($properties as $property)
+                <div class="swiper-slide">
+                    <div class="relative h-[31rem] w-full rounded-2xl shadow-lg overflow-hidden">
+                        <img src="{{ asset('storage/' . $property->image) }}" alt="" 
+                            class="absolute w-full h-full left-0 top-0 object-cover z-0">
+                        
+                        <div class="absolute bottom-0 left-0 w-full h-96 z-10 bg-gradient-to-t from-slate-900 to-transparent"></div>
+                        
+                        <div class="absolute bottom-10 left-10 z-20 text-white">
+                            <h1 class="text-5xl font-light">{{ $property->name }}</h1>
+                            <p class="mt-2">{{ $property->project }}</p>
+                            <x-button-link href="imovel/{{ normalizeString($property->name) }}/{{ $property->id }}">Saiba mais</x-button-link>
+                        </div>
                     </div>
                 </div>
+                @endforeach
             </div>
-            @endforeach
+
+            <div class="swiper-pagination" id="swiper-pagination"></div>
+
+            <div class="swiper-button-next" id="swiper-button-next"></div>
+            <div class="swiper-button-prev" id="swiper-button-prev"></div>
         </div>
-
-        <div class="swiper-pagination" id="swiper-pagination"></div>
-
-        <div class="swiper-button-next" id="swiper-button-next"></div>
-        <div class="swiper-button-prev" id="swiper-button-prev"></div>
-    </div>
+    </section>
 @else
 <section class="w-full min-h-screen relative flex justify-start items-end px-12 py-10 pb-5">
     <div class="swiper w-full" id="slide-home">
         <div class="swiper-wrapper">
             @for ($i = 0; $i <= 4; $i++)
-            <div class="swiper-slide">
-                <div class="relative h-[31rem] w-full rounded-2xl shadow-lg overflow-hidden">
-                    <img src="{{ asset('storage/img/img-example.png') }}" alt="" 
-                        class="absolute w-full h-full left-0 top-0 object-cover z-0">
-                    
-                    <div class="absolute bottom-0 left-0 w-full h-96 z-10 bg-gradient-to-t from-slate-900 to-transparent"></div>
-                    
-                    <div class="absolute bottom-10 left-10 z-20 text-white">
-                        <h1 class="text-5xl font-light">AJF Imóveis</h1>
-                        <p class="mt-2">Os principais lançamentos do Rio de Janeiro você encontra aqui.</p>
+                <div class="swiper-slide">
+                    <div class="relative h-[31rem] w-full rounded-2xl shadow-lg overflow-hidden">
+                        <img src="{{ asset('storage/img/img-example.png') }}" alt="" 
+                            class="absolute w-full h-full left-0 top-0 object-cover z-0">
+                        
+                        <div class="absolute bottom-0 left-0 w-full h-96 z-10 bg-gradient-to-t from-slate-900 to-transparent"></div>
+                        
+                        <div class="absolute bottom-10 left-10 z-20 text-white">
+                            <h1 class="text-5xl font-light">AJF Imóveis</h1>
+                            <p class="mt-2">Os principais lançamentos do Rio de Janeiro você encontra aqui.</p>
+                        </div>
                     </div>
                 </div>
-            </div>
             @endfor
         </div>
+
         <div class="swiper-pagination" id="swiper-pagination"></div>
 
         <div class="swiper-button-next" id="swiper-button-next"></div>
