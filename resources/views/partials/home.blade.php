4183505efcf47ed037060400fc7dfcc9cd77528c
Kaique Nocetti <kaiquenocetti07@gmail.com>
2025-03-07 00:51:25 -0300

    Fix homepage slider layout and adjust spacing in property cards

--- resources/views/partials/home.blade.php
+++ resources/views/partials/home.blade.php
@@ -1,7 +1,7 @@
 @if(!empty($properties[0]))
     <section class="w-full h-screen relative flex justify-start items-end px-12 py-10 pb-5">
-        <div class="swiper w-full h-full" id="slide-home">
-            <div class="swiper-wrapper h-full pt-28">
+        <div class="swiper w-full h-full pt-28" id="slide-home">
+            <div class="swiper-wrapper h-full">
                 @foreach ($properties as $property)
                 <div class="swiper-slide">
                     <div class="relative h-full w-full rounded-2xl shadow-lg overflow-hidden">
@@ -12,7 +12,7 @@
                         
                         <div class="absolute bottom-10 left-10 z-20 text-white">
                             <h1 class="text-5xl font-light">{{ $property->name }}</h1>
-                            <p class="mt-2">{{ $property->project }}</p>
+                            <p class="mt-2 mb-6">{{ $property->project }}</p>
                             <x-button-link class="mt-2" href="imovel/{{ normalizeString($property->name) }}/{{ $property->id }}">Saiba mais</x-button-link>
                         </div>
                     </div>
